42b98d35981b213bfab92965475f6478b4bc9a3a
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2026-04-14 18:57:33 -0400

    added access to API Key inside of Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+string? apiKey = config["StockApiKey"];
