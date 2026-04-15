792733bf38f42cbdf8baee3fcbd63c722fbd88c0
Aditya Mukila <66318348+ZephyrIO@users.noreply.github.com>
2026-04-15 19:01:36 -0400

    Get API Request (#2)
    * added request for stock ticker to main program
    
    * created StockPriceService.cs
    
    * defined default constructor for StockPriceService
    
    * added stockPriceService declariation to program
    
    * defined GerStockPricesAsync method
    
    * Program now can access API results
    
    * removed Console.Write test

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
+using StockTrader.Models;
+using StockTrader.Services;
 
 var config = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
     .Build();
 
 string? apiKey = config["StockApiKey"];
+
+Console.Write("Input the Ticker Symbol (e.g. AAPL for Apple) for the stock you wish to evaluate: ");
+string? tickerSymbol = Console.ReadLine();
+
+var stockPriceService = new StockPriceService(apiKey, tickerSymbol);
+List<StockPrice> data = await stockPriceService.GetStockPricesAsync();
