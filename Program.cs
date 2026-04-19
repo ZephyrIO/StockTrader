d1c8438b739506598923a5c24c51a05c5ba3f0ec
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2026-04-19 17:35:17 -0400

    added console.writes to keep user informed

--- Program.cs
+++ Program.cs
@@ -10,9 +10,11 @@ string? apiKey = config["StockApiKey"];
 
 Console.Write("Input the Ticker Symbol (e.g. AAPL for Apple) for the stock you wish to evaluate: ");
 string? tickerSymbol = Console.ReadLine();
+Console.WriteLine($"Getting {tickerSymbol} Data...");
 
 var stockPriceService = new StockPriceService(apiKey, tickerSymbol);
 List<StockPrice> data = await stockPriceService.GetStockPricesAsync();
+Console.WriteLine("Determining Course of Action...");
 
 if (data[1].AdjClose > data[0].AdjClose)
 {
