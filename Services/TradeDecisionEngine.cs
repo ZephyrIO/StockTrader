ae8221b488782f1ba534450369801ac7fbd168b7
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2026-04-22 15:14:47 -0400

    added empty features list

--- Services/TradeDecisionEngine.cs
+++ Services/TradeDecisionEngine.cs
@@ -5,4 +5,5 @@ namespace StockTrader.Services;
 public class TradeDecisionEngine (List<StockPrice> data)
 {
     private readonly List<StockPrice> _data = data;
+    private readonly List<StockFeatures> _features = new List<StockFeatures>();
 }
