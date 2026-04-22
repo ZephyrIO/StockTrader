5bc04dde2be3bca6bcd65bba7e3b1ac7c72053fc
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2026-04-22 15:36:21 -0400

    added OHLCV data to CalculateFeatures

--- Services/TradeDecisionEngine.cs
+++ Services/TradeDecisionEngine.cs
@@ -5,5 +5,17 @@ namespace StockTrader.Services;
 public class TradeDecisionEngine (List<StockPrice> data)
 {
     private readonly List<StockPrice> _data = data;
-    private readonly List<StockFeatures> _features = new List<StockFeatures>();
+    private List<StockFeatures> _features = new List<StockFeatures>();
+
+    private void CalculateFeatures(int i)
+    {
+        StockFeatures features = new StockFeatures();
+        
+        // Copy Raw OHLCV from _data
+        features.AdjOpen = _data[i].AdjOpen;
+        features.AdjHigh = _data[i].AdjHigh;
+        features.AdjLow = _data[i].AdjLow;
+        features.AdjClose = _data[i].AdjClose;
+        features.Volume = (float) _data[i].Volume;
+    }
 }
