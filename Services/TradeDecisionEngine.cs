07c0b81bd903f18302496ac2457204c86e8d5e55
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2026-04-22 16:20:39 -0400

    created SMA10 function

--- Services/TradeDecisionEngine.cs
+++ Services/TradeDecisionEngine.cs
@@ -12,10 +12,31 @@ public class TradeDecisionEngine (List<StockPrice> data)
         StockFeatures features = new StockFeatures();
         
         // Copy Raw OHLCV from _data
-        features.AdjOpen = _data[i].AdjOpen;
-        features.AdjHigh = _data[i].AdjHigh;
-        features.AdjLow = _data[i].AdjLow;
-        features.AdjClose = _data[i].AdjClose;
+        features.AdjOpen = (float) _data[i].AdjOpen;
+        features.AdjHigh = (float) _data[i].AdjHigh;
+        features.AdjLow = (float) _data[i].AdjLow;
+        features.AdjClose = (float) _data[i].AdjClose;
         features.Volume = (float) _data[i].Volume;
+
+        // Determine number of elements between current element and oldest element
+        int distance = _data.Count - i;
+
+        // If distance is greater than the number of elements needed to calculate the value, calculate the value
+        if (distance > 10)
+        {
+            features.SMA10 = CalcSMA10(i);
+        }
+    }
+
+    private float CalcSMA10(int i)
+    {
+        float total = 0.0f;
+        for (int k = i; k < (i + 10); k++)
+        {
+            total += (float) _data[k].AdjClose;
+        }
+
+        float SMA10 = total / 10;
+        return SMA10;
     }
 }
