65ebcf3191c67caace4e253d821113c6e5b329b7
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2026-04-23 15:09:11 -0400

    simplified the CALCSMA functions into 1

--- Services/TradeDecisionEngine.cs
+++ Services/TradeDecisionEngine.cs
@@ -24,67 +24,31 @@ public class TradeDecisionEngine (List<StockPrice> data)
         // If distance is greater than the number of elements needed to calculate the value, calculate the value
         if (distance >= 10)
         {
-            features.SMA10 = CalcSMA10(pos);
+            features.SMA10 = CalcSMA(pos, 10);
         }
         if (distance >= 20)
         {
-            features.SMA20 = CalcSMA20(pos);
+            features.SMA20 = CalcSMA(pos, 20);
         }
         if (distance >= 50)
         {
-            features.SMA50 = CalcSMA50(pos);
+            features.SMA50 = CalcSMA(pos, 50);
         }
         if (distance >= 200)
         {
-            features.SMA200 = CalcSMA200(pos);
+            features.SMA200 = CalcSMA(pos, 200);
         }
     }
 
-    private float CalcSMA10(int pos)
+    private float CalcSMA(int pos, int window)
     {
         float total = 0.0f;
-        for (int k = pos; k < (pos + 10); k++)
+        for (int k = pos; k < (pos + window); k++)
         {
             total += (float) _data[k].AdjClose;
         }
 
-        float SMA10 = total / 10;
+        float SMA10 = total / window;
         return SMA10;
-    }
-
-    private float CalcSMA20(int pos)
-    {
-        float total = 0.0f;
-        for (int k = pos; k < (pos + 20); k++)
-        {
-            total += (float) _data[k].AdjClose;
-        }
-
-        float SMA20 = total / 20;
-        return SMA20;
-    }
-
-    private float CalcSMA50(int pos)
-    {
-        float total = 0.0f;
-        for (int k = pos; k < (pos + 50); k++)
-        {
-            total += (float) _data[k].AdjClose;
-        }
-
-        float SMA50 = total / 50;
-        return SMA50;
-    }
-
-    private float CalcSMA200(int pos)
-    {
-        float total = 0.0f;
-        for (int k = pos; k < (pos + 200); k++)
-        {
-            total += (float) _data[k].AdjClose;
-        }
-
-        float SMA200 = total / 200;
-        return SMA200;
     }
 }
