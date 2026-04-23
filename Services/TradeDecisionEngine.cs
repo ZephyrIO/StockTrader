f7a48cfaea90dc09adf24403b732907fbf57fbb3
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2026-04-23 14:49:32 -0400

    renamed variables for clarity

--- Services/TradeDecisionEngine.cs
+++ Services/TradeDecisionEngine.cs
@@ -7,43 +7,43 @@ public class TradeDecisionEngine (List<StockPrice> data)
     private readonly List<StockPrice> _data = data;
     private List<StockFeatures> _features = new List<StockFeatures>();
 
-    private void CalculateFeatures(int i)
+    private void CalculateFeatures(int pos)
     {
         StockFeatures features = new StockFeatures();
         
         // Copy Raw OHLCV from _data
-        features.AdjOpen = (float) _data[i].AdjOpen;
-        features.AdjHigh = (float) _data[i].AdjHigh;
-        features.AdjLow = (float) _data[i].AdjLow;
-        features.AdjClose = (float) _data[i].AdjClose;
-        features.Volume = (float) _data[i].Volume;
+        features.AdjOpen = (float) _data[pos].AdjOpen;
+        features.AdjHigh = (float) _data[pos].AdjHigh;
+        features.AdjLow = (float) _data[pos].AdjLow;
+        features.AdjClose = (float) _data[pos].AdjClose;
+        features.Volume = (float) _data[pos].Volume;
 
         // Determine number of elements between current element and oldest element
-        int distance = _data.Count - i;
+        int distance = _data.Count - pos;
 
         // If distance is greater than the number of elements needed to calculate the value, calculate the value
         if (distance >= 10)
         {
-            features.SMA10 = CalcSMA10(i);
+            features.SMA10 = CalcSMA10(pos);
         }
         if (distance >= 20)
         {
-            features.SMA20 = CalcSMA20(i);
+            features.SMA20 = CalcSMA20(pos);
         }
         if (distance >= 50)
         {
-            features.SMA50 = CalcSMA50(i);
+            features.SMA50 = CalcSMA50(pos);
         }
         if (distance >= 200)
         {
-            features.SMA200 = CalcSMA200(i);
+            features.SMA200 = CalcSMA200(pos);
         }
     }
 
-    private float CalcSMA10(int i)
+    private float CalcSMA10(int pos)
     {
         float total = 0.0f;
-        for (int k = i; k < (i + 10); k++)
+        for (int k = pos; k < (pos + 10); k++)
         {
             total += (float) _data[k].AdjClose;
         }
@@ -52,10 +52,10 @@ public class TradeDecisionEngine (List<StockPrice> data)
         return SMA10;
     }
 
-    private float CalcSMA20(int i)
+    private float CalcSMA20(int pos)
     {
         float total = 0.0f;
-        for (int k = i; k < (i + 20); k++)
+        for (int k = pos; k < (pos + 20); k++)
         {
             total += (float) _data[k].AdjClose;
         }
@@ -64,10 +64,10 @@ public class TradeDecisionEngine (List<StockPrice> data)
         return SMA20;
     }
 
-    private float CalcSMA50(int i)
+    private float CalcSMA50(int pos)
     {
         float total = 0.0f;
-        for (int k = i; k < (i + 50); k++)
+        for (int k = pos; k < (pos + 50); k++)
         {
             total += (float) _data[k].AdjClose;
         }
@@ -76,10 +76,10 @@ public class TradeDecisionEngine (List<StockPrice> data)
         return SMA50;
     }
 
-    private float CalcSMA200(int i)
+    private float CalcSMA200(int pos)
     {
         float total = 0.0f;
-        for (int k = i; k < (i + 200); k++)
+        for (int k = pos; k < (pos + 200); k++)
         {
             total += (float) _data[k].AdjClose;
         }
