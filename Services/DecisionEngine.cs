bdaf7698d34b728b9ce4407ebb0b390d29908f1b
Aditya Mukila <66318348+ZephyrIO@users.noreply.github.com>
2026-05-05 17:53:36 -0400

    Create Decision Engine (#5)
    * created DecisionEngine class
    
    * simplified constants in FeatureEngine
    
    * added CreateFeatures call in Program
    
    * added import statements

---
+++ Services/DecisionEngine.cs
@@ -0,0 +1,9 @@
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers.FastTree;
+using Microsoft.ML.TimeSeries;
+
+namespace StockTrader.Services;
+
+public class DecisionEngine
+{}
