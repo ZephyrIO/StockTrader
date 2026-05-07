16b583af87561d64d845fb859e733efbebf72159
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2026-05-07 16:01:51 -0400

    added class variables and created Training method

--- Services/DecisionEngine.cs
+++ Services/DecisionEngine.cs
@@ -6,5 +6,12 @@ using StockTrader.Models;
 
 namespace StockTrader.Services;
 
-public class DecisionEngine
-{}
+public class DecisionEngine (List<StockFeatures> features)
+{
+    private readonly List<StockFeatures> _features = features;
+    private readonly MLContext _mlContext = new MLContext(seed: 0);
+
+    public string TrainAndPredict()
+    {}
+
+}
