801f25618b050d42511d6aa6f50ded27751ec975
Aditya Mukila <66318348+ZephyrIO@users.noreply.github.com>
2026-05-11 14:43:55 -0400

    Implementing AI (#6)
    * added StockTrader.Models import
    
    * added class variables and created Training method
    
    * create dataView and deature columns in for training
    
    * defined training pipeline
    
    * seperate features list into training and testing data
    
    * train the model
    
    * evaluate the model on test data and inform user of model accuracy
    
    * created StockPredition class
    
    * predict the future data point and output the result of the program
    
    * cleaned up spacing

--- Services/DecisionEngine.cs
+++ Services/DecisionEngine.cs
@@ -1,9 +1,77 @@
 using Microsoft.ML;
 using Microsoft.ML.Data;
-using Microsoft.ML.Trainers.FastTree;
 using Microsoft.ML.TimeSeries;
+using Microsoft.ML.Trainers.FastTree;
+using StockTrader.Models;
 
 namespace StockTrader.Services;
 
-public class DecisionEngine
-{}
+public class DecisionEngine (List<StockFeatures> features)
+{
+    private readonly List<StockFeatures> _features = features;
+    private readonly MLContext _mlContext = new MLContext(seed: 0);
+
+    public string TrainAndPredict()
+    {
+        // Load the features list into an ML.NET data view
+        IDataView dataView = _mlContext.Data.LoadFromEnumerable(_features);
+
+        // Define the feature columns that the model will learn from
+        string[] featureColumns = [
+            nameof(StockFeatures.AdjOpen),
+            nameof(StockFeatures.AdjHigh),
+            nameof(StockFeatures.AdjLow),
+            nameof(StockFeatures.AdjLow),
+            nameof(StockFeatures.AdjClose),
+            nameof(StockFeatures.Volume),
+            nameof(StockFeatures.SMA10),
+            nameof(StockFeatures.SMA20),
+            nameof(StockFeatures.SMA50),
+            nameof(StockFeatures.SMA200),
+            nameof(StockFeatures.EMA12),
+            nameof(StockFeatures.EMA26),
+            nameof(StockFeatures.MACD),
+            nameof(StockFeatures.MACDSignal),
+            nameof(StockFeatures.RSI14),
+            nameof(StockFeatures.ROC10),
+            nameof(StockFeatures.ATR14),
+            nameof(StockFeatures.BollingerUpper),
+            nameof(StockFeatures.BollingerMiddle),
+            nameof(StockFeatures.BollingerLower),
+            nameof(StockFeatures.OBV),
+            nameof(StockFeatures.VolumeSMA20)
+        ];
+
+        // Define the training pipeline
+        var pipeline = _mlContext.Transforms.Conversion.MapValueToKey("Label")
+            .Append(_mlContext.Transforms.Concatenate("Features", featureColumns))
+            .Append(_mlContext.MulticlassClassification.Trainers.OneVersusAll(
+                _mlContext.BinaryClassification.Trainers.FastTree(
+                    numberOfLeaves: 20,
+                    numberOfTrees: 100,
+                    minimumExampleCountPerLeaf: 5)))
+            .Append(_mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+        // Split data chronologically into training and testing sets
+        int trainSize = (int)(_features.Count * 0.8);
+        List<StockFeatures> trainData = _features.Take(trainSize).ToList();
+        List<StockFeatures> testData = _features.Skip(trainSize).ToList();
+        IDataView trainView = _mlContext.Data.LoadFromEnumerable(trainData);
+        IDataView testView = _mlContext.Data.LoadFromEnumerable(testData);
+
+        // Train the model on the training set
+        ITransformer model = pipeline.Fit(trainView);
+
+        // Evaluate the model on the test set
+        IDataView predictions = model.Transform(testView);
+        var metrics = _mlContext.MulticlassClassification.Evaluate(predictions);
+        Console.WriteLine($"Model Accuracy: {metrics.MacroAccuracy:P2}");
+
+        // Predict on the most recent data point
+        var predictionEngine = _mlContext.Model.CreatePredictionEngine<StockFeatures, StockPrediction>(model);
+        StockFeatures latestFeatures = _features[^1];
+        StockPrediction prediction = predictionEngine.Predict(latestFeatures);
+
+        return prediction.PredictedLabel;
+    }
+}
