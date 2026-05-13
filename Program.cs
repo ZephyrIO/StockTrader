34031f22d43b3b0becc962529f5b3251c5ea2787
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2026-05-13 15:52:22 -0400

    deleted contents of Program

--- Program.cs
+++ Program.cs
@@ -1,28 +1,2 @@
-﻿using Microsoft.Extensions.Configuration;
-using StockTrader.Models;
+﻿using StockTrader.Models;
 using StockTrader.Services;
-
-var config = new ConfigurationBuilder()
-    .AddUserSecrets<Program>()
-    .Build();
-
-string? apiKey = config["StockApiKey"];
-
-Console.Write("Input the Ticker Symbol (e.g. AAPL for Apple) for the stock you wish to evaluate: ");
-string? tickerSymbol = Console.ReadLine();
-Console.WriteLine($"Getting {tickerSymbol} Data...");
-
-var stockPriceService = new StockPriceService(apiKey, tickerSymbol);
-List<StockPrice> data = await stockPriceService.GetStockPricesAsync();
-
-Console.WriteLine("Calculating Stock Features...");
-
-var featureEngine = new FeatureEngine(data);
-featureEngine.CreateFeatures();
-featureEngine.LabelFeatures();
-
-Console.WriteLine("Building Decision Model...");
-
-var decisionModel = new DecisionEngine(featureEngine.Features);
-string decision = decisionModel.TrainAndPredict();
-Console.WriteLine($"Decision: {decision}");
